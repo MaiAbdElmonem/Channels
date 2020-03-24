b8b7399962aef276abcad1d269f6b1a04a5e0259
MaiAbdElmonem <mai.abdelmonem@ibtikar.net.sa>
2020-03-24 12:54:22 +0200

    remove storyboard

--- Channels/AppDelegate/AppDelegate.swift
+++ Channels/AppDelegate/AppDelegate.swift
@@ -10,16 +10,19 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+  
+ internal var window: UIWindow?
+  
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
-    return true
+     AppManager.launchApp(application)
+           self.window = AppManager.shared.window
+       return true
   }
 
   // MARK: UISceneSession Lifecycle
-
+  @available(iOS 13.0, *)
   func application(
     _ application: UIApplication,
     configurationForConnecting
@@ -29,7 +32,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Use this method to select a configuration to create the new scene with.
     return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
   }
-
+  
+  @available(iOS 13.0, *)
   func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     // Called when the user discards a scene session.
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
---
+++ Channels/Coordinators/Container.swift
@@ -0,0 +1,16 @@
+//
+//  Container.swift
+//  Channels
+//
+//  Created by Mai Abd Elmonem on 3/24/20.
+//  Copyright © 2020 Mai Abd Elmonem. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Container {
+  class func getHomeScene() -> UIViewController {
+         return HomeViewController()
+     }
+}

e468330169f53f9245da54a7cc9de8b953ed4015
MaiAbdElmonem <mai.abdelmonem@ibtikar.net.sa>
2020-06-29 00:33:44 +0200

    fix warnings

--- Channels/Coordinators/Router.swift
+++ Channels/Coordinators/Router.swift
@@ -17,7 +17,5 @@ class Router {
   
   class func getHomeScene() -> UIViewController {
     return HomeModule.createModule()
-//   return HomeViewController()
-//         return ChannelViewController()
      }
 }
